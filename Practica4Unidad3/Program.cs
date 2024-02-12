c37449b7d958fe6d3c3f38cd559bd6fc568302d2
estefivilla07@gmail.com <estefivilla07@gmail.com>
2024-02-12 16:05:44 -0600

    Agregar archivos de proyecto.

---
+++ Practica4Unidad3/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] materias = new int[5];
+Console.WriteLine("Agrega tu calificacion de las siguientes materias");
+Console.WriteLine("Español:");
+materias[0] = int.Parse(Console.ReadLine());
+Console.WriteLine("Matematicas");
+materias[1] = int.Parse(Console.ReadLine());
+Console.WriteLine("Quimica");
+materias[2] = int.Parse(Console.ReadLine());
+Console.WriteLine("Ciencias");
+materias[3] = int.Parse(Console.ReadLine());
+Console.WriteLine("Historia");
+materias[4] = int.Parse(Console.ReadLine());
+
+int suma = 0;
+for (int i = 0; i < materias.Length; i++)
+{
+    suma += materias[i];
+}
+int resultado = suma * 2;
+Console.WriteLine("La suma de las calificaciones es: " + resultado);
+
+if (resultado > 70)
+{
+    Console.WriteLine("Aprobado!!");
+}
+else
+{
+    Console.WriteLine("Reprobado :(");
+}
+Console.ReadKey();
